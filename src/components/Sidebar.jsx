88fb285ee8075c147a34b8bf24260aeb3e26e06d
Derek Mundondo <mundondodt@gmail.com>
2024-02-19 11:59:30 +0200

    Smart factory components initial commit.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState, useEffect } from "react"
 import { ChevronFirst, ChevronLast, MoreVertical } from "lucide-react";
 
-import appLogo from '../assets/hexLab.svg';
+import appLogo from '../assets/lumotechLogo.svg';
 
 const SidebarContext = createContext()
 
@@ -33,20 +33,27 @@ export default function Sidebar({ children }) {
                 <div className="p-4 pb-2 flex justify-between items-center">
                     <img 
                         src={appLogo} 
-                        className={`overflow-hidden transition-all ${expanded ? "w-32" : "w-0"}`}
+                        className={`overflow-hidden transition-all ${expanded ? "w-16" : "w-0"}`}
                         alt=""                    
                     />
+                    {
+                        expanded && 
+                        <div className="flex flex-row bg-gray-700 rounded-tl-2xl rounded-br-2xl py-2 px-3">
+                            <span className="text-xl font-semibold text-white">Heat Staking</span>                   
+                        </div>
+                    }
+                    
                     <button onClick={() => setExpanded(curr => !curr)} className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100">
-                        {expanded ? <ChevronFirst /> : <ChevronLast className="relative right-3"/>}            
+                        {expanded ? <ChevronFirst /> : <ChevronLast />}            
                     </button>
                 </div>
 
+                <hr className="mt-3"/>
+
                 <SidebarContext.Provider value={{expanded}}>
-                    <ul className="flex-1 px-3">{ children }</ul>
+                    <ul className="flex-1 px-3 mt-6">{ children }</ul>
                 </SidebarContext.Provider>    
-
-                <div>Width: {windowWidth} Height: {windowHeight}</div>            
-
+       
                 <div className="border-t flex p-3">
                     <img 
                         src="https://mighty.tools/mockmind-api/content/human/46.jpg"
@@ -69,7 +76,7 @@ export default function Sidebar({ children }) {
 export function SidebarItem({ icon, text, active, alert }) {
     const { expanded } = useContext(SidebarContext)
     return (
-        <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group
+        <li className={`flex relative items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group  
             ${
                 active ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
                 :
@@ -77,13 +84,13 @@ export function SidebarItem({ icon, text, active, alert }) {
             }
         `}>
             {icon}
-            <span className={`overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}`}>{text}</span>
+            { expanded && <span className={`overflow-hidden transition-all ${expanded ? "w-48 ml-3" : "w-0"}`}>{text}</span> }            
             {alert && <div className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? "": "left-8 top-2"}`}></div>}
 
             {
                 !expanded && 
                     <div 
-                        className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm 
+                        className={`absolute left-full rounded-md px-2 w-36 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm 
                         invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100
                         group-hover:translate-x-0`}
                     >
